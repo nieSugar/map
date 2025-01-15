31f04a71fe7b60725c9320492713848e9473aa74
NieRiFeng <17673376360@163.com>
2025-01-15 10:06:43 +0800

    修改弹窗内容

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import { EChartsType, DataZoomComponentOption } from 'echarts';
-import { computed, nextTick, ref } from 'vue';
+import { EChartsType, DataZoomComponentOption } from "echarts";
+import { computed, nextTick, ref } from "vue";
 import * as echarts from "echarts";
+import BJD1 from "../assets/bjd1.png?url";
+import BJD2 from "../assets/bjd2.png?url";
+
 
 const center = ref({ lng: 0, lat: 0 });
 const zoom = ref(15);
@@ -9,12 +12,11 @@ const dialogVisible = ref(false);
 
 const icon = computed(() => {
   return {
-    url: status.value > 0 ? '/red.gif' : '/green.svg',
-    size: { width: 31, height: 40 }
-  }
+    url: status.value > 0 ? "/red.gif" : "/green.svg",
+    size: { width: 31, height: 40 },
+  };
 });
 
-const imgUrl = ref("https://img.lifesugar.top/file/86478f0582c74c7dd8941.jpg");
 const status = ref(0);
 
 let dataZoomChange = false;
@@ -23,38 +25,38 @@ const handler = () => {
   center.value.lng = 121.40953;
   center.value.lat = 31.260756;
   zoom.value = 15;
-}
+};
 
 const initDialog = () => {
   dialogVisible.value = true;
   initEcharts();
-}
+};
 
 function initEcharts() {
   nextTick(() => {
     statusEchart = echarts.init(statusElement.value as HTMLDivElement);
     statusEchart.setOption({
       title: {
-        text: '安全用电状态监控',
-        left: 'center'
+        text: "安全用电状态监控",
+        left: "center",
       },
       grid: {
         left: 80,
-        top: 35
+        top: 35,
       },
       tooltip: {
-        show: false
+        show: false,
       },
-      color: ['#5470c6'],
+      color: ["#5470c6"],
       xAxis: {
-        type: 'category',
+        type: "category",
         data: timeDatas,
         axisLine: {
-          show: false
+          show: false,
         },
         axisTick: {
-          show: false
-        }
+          show: false,
+        },
       },
       // visualMap: {
       //   show: false,
@@ -68,36 +70,53 @@ function initEcharts() {
       //   }]
       // },
       yAxis: {
-        type: 'value',
+        type: "value",
         min: 0,
         max: 1,
         interval: 1,
         axisLabel: {
           formatter: (value: number) => {
-            return value === 1 ? '充电' : '正常';
-          }
-        }
+            return value === 1 ? "报警" : "正常";
+          },
+        },
       },
       series: [
         {
           data: stateDatas,
-          type: 'line',
+          type: "line",
           itemStyle: {
             color: (params: any) => {
-              return params.value === 1 ? '#ff0000' : '#5470c6';
-            }
-          }
-        }
+              return params.value === 1 ? "#ff0000" : "#5470c6";
+            },
+          },
+        },
       ],
-      dataZoom: [{
-        show: true,
-        start: 100 - (20 / timeDatas.length * 100),
-        end: 100
-      }]
+      dataZoom: [
+        {
+          show: true,
+          height: 10,
+          start: 100 - (20 / timeDatas.length) * 100,
+          end: 100,
+        },
+      ],
     });
-    statusEchart.on('dataZoom', () => {
+    statusEchart.on("dataZoom", () => {
       dataZoomChange = true;
-    })
+    });
+
+    statusPie = echarts.init(statusPieElement.value as HTMLDivElement);
+    statusPie.setOption({
+      series: [
+        {
+          type: "pie",
+          radius: "50%",
+          data: [
+            { value: 0, name: "正常" },
+            { value: 0, name: "报警" },
+          ],
+        },
+      ],
+    });
   });
 }
 
@@ -118,11 +137,15 @@ const statusElement = ref<HTMLDivElement | null>(null);
 let timeDatas: Array<string> = [];
 let stateDatas: Array<number> = [];
 let statusEchart: EChartsType | null;
-let work = new Worker('/workers/unitySignalr.js');
+
+const statusPieElement = ref<HTMLDivElement | null>(null);
+let statusPie: EChartsType | null;
+
+let work = new Worker("/workers/unitySignalr.js");
 
 work.onmessage = (ev) => {
   mapEchartsData(ev.data as EchartData);
-}
+};
 
 function mapEchartsData(data: EchartData) {
   const { max, response } = data;
@@ -137,10 +160,25 @@ function mapEchartsData(data: EchartData) {
   }
   if (dialogVisible.value) {
     updateEcartOptionData(statusEchart!, stateDatas, timeDatas);
+    updateEchartsPie();
   }
 }
 
-function updateEcartOptionData(item: EChartsType, data: Array<number>, time: Array<string>) {
+function updateEchartsPie() {
+  stateDatas.push();
+  statusPie?.setOption({
+    series: [
+      { value: stateDatas.filter((item) => item === 0).length, name: "正常" },
+      { value: stateDatas.filter((item) => item === 1).length, name: "报警" },
+    ],
+  });
+}
+
+function updateEcartOptionData(
+  item: EChartsType,
+  data: Array<number>,
+  time: Array<string>
+) {
   const newOption: echarts.EChartsOption = {
     xAxis: {
       data: time,
@@ -148,57 +186,66 @@ function updateEcartOptionData(item: EChartsType, data: Array<number>, time: Arr
     series: [
       {
         data: data,
-        type: 'line'
-      }
-    ]
+        type: "line",
+      },
+    ],
   };
   if (time.length > 20 && !dataZoomChange) {
     const option = item.getOption();
-    const dataZoom = (option.dataZoom as Array<DataZoomComponentOption>);
+    const dataZoom = option.dataZoom as Array<DataZoomComponentOption>;
     if (dataZoom.length > 0 && dataZoom[0].end === 100) {
-      newOption.dataZoom = [{
-        show: true,
-        start: 100 - (20 / time.length * 100),
-        end: 100
-      }];
+      newOption.dataZoom = [
+        {
+          show: true,
+          height: 15,
+          start: 100 - (20 / time.length) * 100,
+          end: 100,
+        },
+      ];
     }
   }
   item?.setOption(newOption);
 }
 
 // #endregion
-
 </script>
 
 <template>
   <div class="title">
-    <div class="subTitle"> 海仿智能安全用电卫士系统 </div>
+    <div class="subTitle">海仿智能安全用电卫士系统</div>
   </div>
   <div class="card">
     <baidu-map class="bm-view" :zoom="zoom" :center="center" @ready="handler">
-      <bm-marker :position="{ lng: 121.40953, lat: 31.260756 }" :icon="icon" @click="initDialog">
+      <bm-marker
+        :position="{ lng: 121.40953, lat: 31.260756 }"
+        :icon="icon"
+        @click="initDialog"
+      >
       </bm-marker>
     </baidu-map>
-    <el-dialog v-model="dialogVisible" width="40%" :class="{ 'error-shadow': status === 1 }">
+    <el-dialog
+      v-model="dialogVisible"
+      width="40%"
+      :class="{ 'error-shadow': status === 1 }"
+    >
       <template #header>
-        <div class="header">
-          海仿智能安全用电卫士系统
-        </div>
+        <div class="header">海仿智能安全用电卫士系统</div>
       </template>
       <div class="main">
-        <div>
-          <el-image class="img" fit="contain" :src="imgUrl"></el-image>
-        </div>
         <div class="title-info">
           <div class="title-info-row">
-            <label class="lable">小区: </label><span class="lable">塔普合欢苑小区东区</span>
-          </div>
-          <div class="title-info-row">
-            <label class="lable">
-              地址:
-            </label>
+            <label class="lable"> 地址: </label>
             <span class="lable">上海市普陀区桃浦新村合欢苑121号6楼-601</span>
           </div>
+          <div class="title-info-row">
+            <label class="lable"> 状态: </label>
+            <span class="lable">
+              <img class="bg-img" :src="status === 1 ? BJD1 : BJD2" alt="" />
+            </span>
+          </div>
+        </div>
+        <div class="title-info">
+          <div ref="statusPieElement" class="echats"></div>
         </div>
       </div>
       <div class="body">
@@ -255,14 +302,13 @@ function updateEcartOptionData(item: EChartsType, data: Array<number>, time: Arr
   transform: skew(-10deg);
 }
 
-
 .bm-view {
   width: 100%;
   height: 100vh;
 }
 
 .title {
-  background-image: url('/imgs/bg.png');
+  background-image: url("/imgs/bg.png");
   background-repeat: no-repeat;
   background-position: center;
   position: absolute;
@@ -282,6 +328,11 @@ function updateEcartOptionData(item: EChartsType, data: Array<number>, time: Arr
   color: #fff;
 }
 
+.bg-img {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
 .main {
   display: flex;
 }
@@ -297,14 +348,14 @@ function updateEcartOptionData(item: EChartsType, data: Array<number>, time: Arr
   display: grid;
   align-items: center;
   width: calc(100% - 300px - 4rem);
-  grid-gap: 20px;
 }
 
 .title-info-row {
   display: flex;
+  align-items: center;
 }
 
-.title-info-row>label {
+.title-info-row > label {
   min-width: 3rem;
 }
 
